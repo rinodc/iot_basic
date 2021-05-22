d32d961cc5681ac3035c42717e7c66925ddc8883
dc <rinodc13@gmail.com>
2021-05-22 09:48:02 +0700

    delete not required

--- esp32_jarak_hcsr04_local_logic_ubidots_mqtt/Servo.ino
+++
@@ -1,7 +0,0 @@
-void changeServoPos()
-{
-  sscanf((char*)sub_payload, "%i", &servoPos);
-  String buf = "Servo Pos set to = " + String(servoPos) + " °";
-  Serial.println(buf);
-//  servo.write(servoPos);
-}
